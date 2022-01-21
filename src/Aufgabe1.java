59d7960f2e78a9c72b08fb3f6fb0179ebc9eb575
JoJokoma <71552608+JoJokoma@users.noreply.github.com>
2022-01-21 18:04:43 +0100

    Main java

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,10 +1,15 @@
+import java.io.*;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import static java.lang.Integer.parseInt;
+
+
 public class Aufgabe1 {
+    static File filmfile = new File("src/files/1000GreatestFilms (1).csv");
     public static void main(String[] args) throws SQLException {
         try {
             Class.forName("org.postgresql.Driver");
@@ -24,7 +29,45 @@ public class Aufgabe1 {
 
         }
         try {
-            String s = "";
+            PreparedStatement filmInsert =  con.prepareStatement("INSERT INTO FILM (TITEL, LAENGE) VALUES (?,?)",Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement genreInsert =  con.prepareStatement("INSERT INTO GENRE (GENRE) VALUES (?)");
+            PreparedStatement filmgenre = con.prepareStatement("INSERT INTO GENREFILM (FILM_NUMMER, GENRE_NUMMER)VALUES(?,?)");
+
+            BufferedReader filmreader = new BufferedReader(new FileReader(filmfile.getAbsolutePath()));
+            int linecount = 0;
+            String infoline = null;
+            filmreader.readLine();
+            int z = 0;
+            while((infoline = filmreader.readLine())!= null ){
+                String[] filmdata = infoline.split(";");
+                String titel = filmdata[3];
+                int length = Integer.valueOf(filmdata[7]);
+                String[] geners = filmdata[8].split("-");
+                filmInsert.setString(1, titel);
+                filmInsert.setInt(2,length);
+                filmInsert.executeUpdate();
+                ResultSet filmKeys = filmInsert.getGeneratedKeys();
+                int size = geners.length;
+                if(filmKeys.next()){
+                int filmkey = filmKeys.getInt(1);
+                for (int i = 0; i<size;i++){
+                   try{
+                    genreInsert.setString(1,geners[i]);
+                    genreInsert.executeUpdate();}
+                   catch(SQLException pw){}
+                    filmgenre.setInt(1,filmkey);
+                    filmgenre.setString(2,geners[i]);
+
+                }
+
+                }
+
+
+
+
+            }
+            filmreader.close();
+            /*String s = "";
             Statement stmt = con.createStatement();
             String query = "SELECT * FROM film";
             ResultSet rs = stmt.executeQuery(query);
@@ -38,10 +81,12 @@ public class Aufgabe1 {
 
 
             }
-            System.out.println(s);
+            System.out.println(s);*/
 
-
-        } catch (SQLException en) {
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
