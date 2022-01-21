84b6654d6e0b87a9845da880e9ccb77268fe2dc3
JoJokoma <71552608+JoJokoma@users.noreply.github.com>
2022-01-21 14:37:08 +0100

    Main java

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -10,21 +10,21 @@ public class Aufgabe1 {
         }
         Connection con = null;
         try {
-            String url = "jdbc:postgresql://db.intern.mi.hs-rm.de:5432/database";
-            con = DriverManager.getConnection(url, "jwebe001", "password");
+            String url = "jdbc:postgresql://db.intern.mi.hs-rm.de:5432/jpors001_kino";
+            con = DriverManager.getConnection(url, "jwebe001", "");
         } catch (SQLException e) {
             System.out.println("Couldn't connect - aborting");
             System.exit(-1);
         }
      try {
          Statement stmt = con.createStatement();
-         String query = "SELECT * FROM studierende";
+         String query = "SELECT * FROM kinoprogram";
          ResultSet rs = stmt.executeQuery(query);
          System.out.println("Studierende: ");
          while (rs.next()) {
-             String s = "- " + rs.getString("vorname");
-             s = s + " " + rs.getString("name");
-             s = s + ", " + rs.getString("matnr");
+             String s = "- " + rs.getString("film");
+             s = s + " " + rs.getString("datum");
+             s = s + ", " + rs.getString("uhrzeit");
              System.exit(-1);
 
 
